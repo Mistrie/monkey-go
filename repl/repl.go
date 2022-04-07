ebc3acb1ac637414c400e7c7b60af358fb770f8a
Jinmyeong Kim <jinmyeong.kim@sap.com>
2022-04-07 21:28:13 +0900

    Initial commit for monkey-go
    - main()
    - Skeleton for repl package

---
+++ repl/repl.go
@@ -0,0 +1,26 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		io.WriteString(out, line)
+		io.WriteString(out, "\n")
+	}
+}
