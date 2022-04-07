f2b2af5fdfa9b159ce551bc3f1e2ee5560954ec5
Jinmyeong Kim <jinmyeong.kim@sap.com>
2022-04-07 22:44:07 +0900

    Define ASTs
    Node
      Statement
      Expression (incl. Literal)

---
+++ ast/ast.go
@@ -0,0 +1,45 @@
+package ast
+
+import (
+	"bytes"
+)
+
+// The base Node interface
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+// All statement nodes implement this
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// All expression nodes implement this
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
