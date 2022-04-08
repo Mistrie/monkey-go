860ab08aecf467afd455cd30b42a125fbce63aff
Jinmyeong Kim <kissesjelly@gmail.com>
2022-04-08 23:56:13 +0900

    Parser: implement statements

--- parser/parser_expression.go
+++ parser/parser_expression.go
@@ -5,3 +5,8 @@ import "monkey/ast"
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	// TODO
+	return nil
+}
