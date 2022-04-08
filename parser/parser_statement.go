eb9f6998f35cd6325417213be58f946f42a56a5f
Jinmyeong Kim <kissesjelly@gmail.com>
2022-04-08 23:46:46 +0900

    Parser: functions for statement

--- parser/parser_statement.go
+++ parser/parser_statement.go
@@ -1,8 +1,29 @@
 package parser
 
-import "monkey/ast"
+import (
+	"monkey/ast"
+	"monkey/token"
+)
 
 func (p *Parser) parseStatement() ast.Statement {
-	// TODO
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
+	default:
+		return p.parseExpressionStatement()
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	return nil
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	return nil
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return nil
 }
