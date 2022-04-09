16487b70f7fb352ca20fe7d5100ff5d98bc24874
Jinmyeong Kim <kissesjelly@gmail.com>
2022-04-09 23:43:29 +0900

    Evaluator: Literals
    Test are not available yet: statements should be evaluated first

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,11 +1,70 @@
 package evaluator
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node, env *object.Environment) object.Object {
-	// TODO
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+	case *ast.BlockStatement:
+	case *ast.ExpressionStatement:
+	case *ast.ReturnStatement:
+	case *ast.LetStatement:
+
+	// Literals
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanOBject(node.Value)
+
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
+
+	// Expressions
+	case *ast.PrefixExpression:
+	case *ast.InfixExpression:
+	case *ast.IfExpression:
+	case *ast.Identifier:
+	case *ast.CallExpression:
+	case *ast.IndexExpression:
+	}
+
 	return nil
+}
+
+func newError(format string, a ...interface{}) *object.Error {
+	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func isError(obj object.Object) bool {
+	if obj != nil {
+		return obj.Type() == object.ERROR_OBJ
+	}
+	return false
 }
