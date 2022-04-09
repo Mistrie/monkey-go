0994ca167d7d7a28ac31ded8291c71579d4e00ef
Jinmyeong Kim <kissesjelly@gmail.com>
2022-04-09 23:43:29 +0900

    Evaluator: Literals
    Test are not available yet: statements should be evaluated first

---
+++ evaluator/evaluator_expression.go
@@ -0,0 +1,11 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	// TODO
+	return nil
+}
